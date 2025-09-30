fbca0274c4c2b2171069a8b4eee483b815358721
denissever <sedihdenis@bk.ru>
2025-09-30 17:48:05 +0300

    commit HelloNewApp

--- src/main/java/main/HelloNewApp.java
+++ src/main/java/main/HelloNewApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloNewApp {
-
     public static void main(String[] args) {
         SpringApplication.run(HelloNewApp.class, args);
     }
